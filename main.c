0eec96b000ced7d20b55638f065463b1c8138c49
FritzSavira <Friedemann.Decker@online.de>
2026-01-07 16:35:03 +0000

    Refactor grid dimensions and update generation logic in main.c

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h> // zur Initialisierung des Zufallszahlengenerators
 
-#define ROWS 35  
-#define COLS 150
+#define ROWS 30  
+#define COLS 30
 
 // Struktur definieren
 typedef struct {
@@ -14,7 +15,7 @@ typedef struct {
 // Prototypen
 void init_world(World *current_gen);
 void print_world(World *current_gen);
-// void update_generation(World current_gen, World next_gen);
+void update_generation(World *current_gen, World *next_gen);
 
 
 int main(void) {
@@ -31,14 +32,14 @@ int main(void) {
     // Zufälliges Muster lebender Zellen
     init_world(current_gen);
 
-
-    // Aktuelle Population ausgeben
-    print_world(current_gen);
-
     // Spiel-Schleife (Loop)
     //while (true) {
-    //    print_world(current_gen);
-    //    update_generation(current_gen, next_gen);
+        // Aktuelle Population ausgeben
+        printf("current_gen:\n");
+        print_world(current_gen);
+        update_generation(current_gen, next_gen);
+        printf("next_gen:\n");
+        print_world(next_gen);
 
         // Tausche die Gitter (Pointer-Zuweisung)
     //    World *temp = current_gen;
@@ -50,15 +51,17 @@ int main(void) {
 
     // Speicher freigeben (Vermeidung von Memory Leaks)
     free(current_gen->grid);
-//    free(next_gen->grid);
+    free(next_gen->grid);
     free(current_gen);
-//    free(next_gen);
+    free(next_gen);
 
     return 0;
 }
 
 void init_world(World *current_gen)
 {
+    // Initialisiere den Zufallszahlengenerator mit der aktuellen Zeit
+    srand(time(NULL));
     for (int i = 0; i < (ROWS * COLS); i++)
     {
         current_gen->grid[i] = rand() % 2; // Zufällige 0 oder 1
@@ -70,11 +73,38 @@ void print_world(World *current_gen)
 {
     for (int i = 0; i < (ROWS * COLS); i++)
     {
-        printf("%i", current_gen->grid[i]);
+        printf("%i  ", current_gen->grid[i]);
         if ((i+1) % COLS == 0)
         {
             printf("\n");
         }
     }
     printf("\n");
+}
+
+void update_generation(World *current_gen, World *next_gen)
+{
+    for (int i = 0; i < (ROWS * COLS); i++)
+    {
+        //next_generation Zellenweise berechnen
+        //Anzahl lebender Zellen zählen
+        int zellen = 
+        current_gen->grid[i - COLS - 1] +
+        current_gen->grid[i - COLS] +
+        current_gen->grid[i - COLS + 1] +
+        current_gen->grid[i - 1] +
+        current_gen->grid[i + 1] +
+        current_gen->grid[i + COLS - 1] +
+        current_gen->grid[i + COLS] +
+        current_gen->grid[i + COLS + 1];
+
+        if (zellen == 2 || zellen == 3)
+        {
+            next_gen->grid[i] = 1;
+        }
+        else
+        {
+            next_gen->grid[i] = 0;
+        }
+    }
 }
